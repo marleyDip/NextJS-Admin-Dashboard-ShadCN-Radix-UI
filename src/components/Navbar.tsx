455adb03b3bd1291b26127696c9d070ea429d897
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-09-21 20:55:16 +0600

    What is cn & implement dropdown-menu and avatar customize

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,63 @@
+import { LogOut, Settings, SunMoon, User } from 'lucide-react';
+import Link from "next/link"
+
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "./ui/dropdown-menu"
+
+
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav className="p-4 flex items-center justify-between">
+            {/* Left */}
+            collapseButton
+            {/* Left */}
+
+            {/* Right */}
+            <div className="flex items-center gap-4">
+                <Link href="/">Dashboard</Link>
+                <SunMoon />
+
+                <DropdownMenu>
+                    <DropdownMenuTrigger>
+                        <Avatar>
+                            <AvatarImage src="https://avatars.githubusercontent.com/u/1486366" />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                    </DropdownMenuTrigger>
+
+                    <DropdownMenuContent sideOffset={10}>
+                        <DropdownMenuLabel>
+                            My Account
+                        </DropdownMenuLabel>
+
+                        <DropdownMenuSeparator />
+
+                        <DropdownMenuItem>
+                            <User className="h-[1.2rem] w-[1.2rem] mr-2" />
+                            Profile
+                        </DropdownMenuItem>
+                        
+                        <DropdownMenuItem>
+                            <Settings className="h-[1.2rem] w-[1.2rem] mr-2" />
+                            Settings
+                        </DropdownMenuItem>
+
+                        <DropdownMenuItem variant="destructive">
+                            <LogOut className="h-[1.2rem] w-[1.2rem] mr-2" />
+                            Logout
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
+            {/* Right */}
+        </nav>
     )
 }
 
