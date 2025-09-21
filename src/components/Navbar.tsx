2f1613d49cc56de4f39b5906f1a16471a59c5e88
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-09-21 22:38:14 +0600

    asChild & sr-only(tailwind css class)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { LogOut, Settings, SunMoon, User } from 'lucide-react';
+import { LogOut, Settings, SquareMenu, SunMoon, User } from 'lucide-react';
 import Link from "next/link"
 
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
@@ -11,7 +11,6 @@ import {
     DropdownMenuTrigger,
 } from "./ui/dropdown-menu"
 
-
 const Navbar = () => {
     return (
         <nav className="p-4 flex items-center justify-between">
@@ -22,13 +21,14 @@ const Navbar = () => {
             {/* Right */}
             <div className="flex items-center gap-4">
                 <Link href="/">Dashboard</Link>
+
                 <SunMoon />
 
                 <DropdownMenu>
                     <DropdownMenuTrigger>
                         <Avatar>
-                            <AvatarImage src="https://avatars.githubusercontent.com/u/1486366" />
-                            <AvatarFallback>CN</AvatarFallback>
+                            <AvatarImage src="https://avatars.githubusercontent.com/u/1486366" alt='user-icon' />
+                            <AvatarFallback>user</AvatarFallback>
                         </Avatar>
                     </DropdownMenuTrigger>
 
@@ -43,7 +43,7 @@ const Navbar = () => {
                             <User className="h-[1.2rem] w-[1.2rem] mr-2" />
                             Profile
                         </DropdownMenuItem>
-                        
+
                         <DropdownMenuItem>
                             <Settings className="h-[1.2rem] w-[1.2rem] mr-2" />
                             Settings
@@ -62,3 +62,27 @@ const Navbar = () => {
 }
 
 export default Navbar
+
+/*  asChild And sr-only (tailwind class)
+
+    => asChild → makes your custom element(like ShadCN Button) the trigger instead of Radix creating its own.
+    => Without asChild, Radix would wrap your < Button > inside another<button> → causing invalid nested buttons.
+    => With asChild, Radix uses your ShadCN<Button> as the actual trigger
+
+    => sr - only → hides text visually but keeps it accessible for screen readers.
+
+    < DropdownMenu >
+        <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+                <SquareMenu />
+                <span className="sr-only">Open Menu</span>
+            </Button>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent>
+            <DropdownMenuItem>Menu Item 1</DropdownMenuItem>
+            <DropdownMenuItem>Menu Item 1</DropdownMenuItem>
+            <DropdownMenuItem>Menu Item 1</DropdownMenuItem>
+        </DropdownMenuContent>
+    </ >
+*/
